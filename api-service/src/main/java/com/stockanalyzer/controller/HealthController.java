6cc88775b0d99d7ecd34b7e5760e53fd2e8ff4d1
Matias Fanucchi <matiasfanucchi99@gmail.com>
2026-04-30 20:12:02 -0300

    add @CrossOrigin to all controllers for Netlify

--- api-service/src/main/java/com/stockanalyzer/controller/HealthController.java
+++ api-service/src/main/java/com/stockanalyzer/controller/HealthController.java
@@ -3,10 +3,12 @@ package com.stockanalyzer.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {org.springframework.http.HttpMethod.GET, org.springframework.http.HttpMethod.POST, org.springframework.http.HttpMethod.PUT, org.springframework.http.HttpMethod.DELETE, org.springframework.http.HttpMethod.OPTIONS})
 public class HealthController {
     
     @GetMapping("/api/health")
